50527091cc378e992d92c22f5962338a0a48e77f
ginanjarn <ginanjarn@gmail.com>
2023-10-07 16:30:54 +0700

    move cursor to rename target

--- pythontools.py
+++ pythontools.py
@@ -991,6 +991,10 @@ class PythontoolsRenameCommand(sublime_plugin.TextCommand):
         cursor = self.view.sel()[0]
         point = event["text_point"] if event else cursor.a
         if HANDLER.ready():
+            # move cursor to point
+            self.view.sel().clear()
+            self.view.sel().add(point)
+
             start_row, start_col = self.view.rowcol(point)
             HANDLER.textdocument_preparerename(file_name, start_row, start_col)
 
